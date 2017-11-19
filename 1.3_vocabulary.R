0054bd3d558fd4c8d81fe5aaed5ca54a2d2937c6
Shade Wilson <sw2ht@virginia.edu>
2017-11-19 15:48:02 -0800

    Import skeleton basic vocab

---
+++ 1.3_vocabulary.R
@@ -0,0 +1,89 @@
+##### Advanced R #####
+# By Hadley Wickham
+# Section 1: FOUNDATIONS
+# Chapter 3: Vocabulary
+
+# BASICS ------------------------------------------------------------------
+
+# The first functions to learn
+# ?
+# str
+
+# Important operators and assignment
+# %in%, match
+# =, <-, <<-
+#   $, [, [[, head, tail, subset
+# with
+# assign, get
+
+# Comparison
+# all.equal, identical
+# !=, ==, >, >=, <, <=
+#   is.na, complete.cases
+# is.finite
+
+# Basic math
+# *, +, -, /, ^, %%, %/%
+#   abs, sign
+# acos, asin, atan, atan2
+# sin, cos, tan
+# ceiling, floor, round, trunc, signif
+# exp, log, log10, log2, sqrt
+# 
+# max, min, prod, sum
+# cummax, cummin, cumprod, cumsum, diff
+# pmax, pmin
+# range
+# mean, median, cor, sd, var
+# rle
+
+# Functions to do with functions
+# function
+# missing
+# on.exit
+# return, invisible
+
+# Logical & sets
+# &, |, !, xor
+# all, any
+# intersect, union, setdiff, setequal
+# which
+
+# Vectors and matrices
+# c, matrix
+# # automatic coercion rules character > numeric > logical
+# length, dim, ncol, nrow
+# cbind, rbind
+# names, colnames, rownames
+# t
+# diag
+# sweep
+# as.matrix, data.matrix
+
+# Making vectors
+# c
+# rep, rep_len
+# seq, seq_len, seq_along
+# rev
+# sample
+# choose, factorial, combn
+# (is/as).(character/numeric/logical/...)
+
+# Lists & data.frames
+# list, unlist
+# data.frame, as.data.frame
+# split
+# expand.grid
+
+# Control flow
+# if, &&, || (short circuiting)
+# for, while
+# next, break
+# switch
+# ifelse
+
+# Apply & friends
+# lapply, sapply, vapply
+# apply
+# tapply
+# replicate
